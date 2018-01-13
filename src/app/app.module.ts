8183863173321295f0852731803d4496fca52020
Antony <Toxa23A@gmail.com>
2018-01-13 21:34:22 +0200

    first card with woble

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpModule } from "@angular/http";
 import { AppRoutingModule } from './app-routing.module';
-
+import { CustomMaterialModule } from "./custom-material/custom-material.module";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { AppComponent } from './app.component';
 
 import { environment } from '../environments/environment';
+import { FormsModule } from '@angular/forms';
+import { CardsModule } from './cards/cards.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        CardsModule,
+
+        FormsModule,
+        HttpModule,
+        CustomMaterialModule,
+        BrowserAnimationsModule,
+        BrowserModule,
+        AppRoutingModule,
+        ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }

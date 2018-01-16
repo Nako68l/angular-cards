6abeb4014c46a1abbdc3c18818b2732051f6ca2a
Antony <Toxa23A@gmail.com>
2018-01-16 21:42:58 +0200

    add swipes suppor and animation update 4 cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, ClassProvider } from '@angular/core';
 import { HttpModule } from "@angular/http";
 import { AppRoutingModule } from './app-routing.module';
 import { CustomMaterialModule } from "./custom-material/custom-material.module";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { AppComponent } from './app.component';
-
 import { environment } from '../environments/environment';
 import { FormsModule } from '@angular/forms';
 import { CardsModule } from './cards/cards.module';
+
+import * as Hammer from 'hammerjs';
+import { HammerGestureConfig, HAMMER_GESTURE_CONFIG } from "@angular/platform-browser";
+
+export class MyHammerConfig extends HammerGestureConfig {
+    overrides = <any>{
+        'swipe': { direction: Hammer.DIRECTION_ALL }
+    }
+}
 
 @NgModule({
     declarations: [
@@ -26,7 +34,10 @@ import { CardsModule } from './cards/cards.module';
         AppRoutingModule,
         ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
     ],
-    providers: [],
+    providers: [{
+        provide: HAMMER_GESTURE_CONFIG,
+        useClass: MyHammerConfig
+    }],
     bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/cards/main-card/main-card.component.ts
+++ src/app/cards/main-card/main-card.component.ts
@@ -8,13 +8,22 @@ import { trigger, transition, keyframes, animate, style } from '@angular/animati
     animations: [
         trigger('cardAnimator', [
             transition('* => swing', animate(1000, keyframes(kf.swing))),
-            transition('* => flip', [
-                style({ transform: 'perspective(400px) rotate3d(0, 1, 0, -360deg)' }),
-                animate(1000),]),
-            transition('* => flipR', animate(1000, keyframes([
+            transition('* => rubberBand', animate(1000, keyframes([
+                style({ transform: 'scale3d(1, 1, 1)', offset: 0 }),
+                style({ transform: 'scale3d(1.25, 0.75, 1)', offset: 0.3 }),
+                style({ transform: 'scale3d(0.75, 1.25, 1)', offset: 0.4 }),
+                style({ transform: 'scale3d(1.15, 0.85, 1)', offset: 0.5 }),
+                style({ transform: 'scale3d(.95, 1.05, 1)', offset: 0.65 }),
+                style({ transform: 'scale3d(1.05, .95, 1)', offset: 0.75 }),
+                style({ transform: 'scale3d(1, 1, 1)', offset: 1 }),
+            ]))),
+            transition('* => flip', animate(750, keyframes([
+                style({ transform: 'perspective(400px) rotate3d(0,1,0,-360deg)' }),
+            ]))),
+            transition('* => flipR', animate(750, keyframes([
                 style({ transform: 'perspective(400px) rotate3d(0,0,1,-360deg)' }),
             ]))),
-            transition('* => flipY', animate(1000, keyframes([
+            transition('* => flipY', animate(750, keyframes([
                 style({ transform: 'perspective(400px) rotate3d(1,0,0,-360deg)' }),
             ]))),
             transition('* => hinge2', animate(2000, keyframes([
@@ -34,10 +43,27 @@ import { trigger, transition, keyframes, animate, style } from '@angular/animati
             transition('* => fadeOut', animate(1000, keyframes([
                 style({ transform: 'translate3d(-100%, 0, 0)', opacity: 0, })
             ]))),
-            transition('* => zoomOutRight', animate(1000, keyframes([
+            transition('* => flyRight', animate(1000, keyframes([
                 style({ transform: 'scale3d(.475, .475, .475) translate3d(375px, 0, 0) perspective(300px) rotate3d(0, 1, 0, -360deg)', opacity: 0, offset: 0.7 }),
                 style({ transform: 'translate3d(-100%, 0, 0)', opacity: 0, offset: 0.71 }),
                 style({ transform: 'none', opacity: 1, offset: 1 }),
+            ]))),
+            transition('* => flyUp', animate(1000, keyframes([
+                style({ transform: 'scale3d(.3, .3, .3) translate3d(0, -375px, 375px) perspective(300px) rotate3d(1, 0, 0, 360deg)', opacity: 0, offset: 0.7 }),
+                // style({ transform: 'translate3d(0, 0, -100%)', opacity: 0, offset: 0.71 }),
+                style({ transform: 'none', opacity: 1, offset: 1 }),
+            ]))),
+            transition('* => flyDown', animate(1000, keyframes([
+                style({ transform: 'perspective(400px) rotate3d(0,0,1,-360deg) scale3d(.475, .475, .475) translate3d(0, -60px, 0)', animationTimingFunction: 'cubic-bezier(0.550, 0.055, 0.675, 0.190)', opacity: 1, offset: 0.4 }),
+                style({ transform: 'perspective(400px) rotate3d(0,0,1,360deg) scale3d(.1, .1, .1) translate3d(0, 2000px, 0)', transformOrigin: 'center bottom', animationTimingFunction: 'cubic-bezier(0.175, 0.885, 0.320, 1)', opacity: 0, offset: 1 }),
+                // style({ transform: 'translate3d(0, 0, -100%)', opacity: 0, offset: 0.71 }),
+                // style({ transform: 'none', opacity: 1, offset: 1 }),
+            ]))),
+            transition('* => rollOut', animate(750, keyframes([
+                style({ opacity: 1, offset: 0 }),
+                style({ transform: 'translate3d(-100%, 0, 0) rotate3d(0, 0, -1, 120deg)', opacity: 0, offset: .7 }),
+                style({ transform: 'translate3d(100%, 0, 0)', opacity: 0, offset: .71 }),
+                style({ transform: 'none', opacity: 1, offset: 1 })
             ]))),
         ])
     ]

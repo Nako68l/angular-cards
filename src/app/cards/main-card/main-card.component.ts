921e858da8c3d7efb3a7ddc313dbf50595f993c1
Antony <Toxa23A@gmail.com>
2018-01-16 19:04:29 +0200

    add swiperight anmation and support

--- src/app/cards/main-card/main-card.component.ts
+++ src/app/cards/main-card/main-card.component.ts
@@ -17,18 +17,6 @@ import { trigger, transition, keyframes, animate, style } from '@angular/animati
             transition('* => flipY', animate(1000, keyframes([
                 style({ transform: 'perspective(400px) rotate3d(1,0,0,-360deg)' }),
             ]))),
-            //TODO: WHYYYY??????
-            //https://daneden.github.io/animate.css/
-            //https://github.com/daneden/animate.css/blob/fb4313ad0c3c2a962f7a86c89cbbc69ba1285214/source/specials/hinge.css
-            // transition('* => hinge', [
-            //     style({ transformOrigin: 'top left', animationTimingFunction: 'ease-in-out', offset: 0 }),
-            //     style({ transformOrigin: 'top left', transform: 'rotate3d(0, 0, 1, 80deg)', animationTimingFunction: 'ease-in-out', offset: '.2' }),
-            //     style({ transformOrigin: 'top left', transform: 'rotate3d(0, 0, 1, 60deg)', animationTimingFunction: 'ease-in-out', opacity: 1, offset: .4, }),
-            //     style({ transformOrigin: 'top left', transform: 'rotate3d(0, 0, 1, 80deg)', animationTimingFunction: 'ease-in-out', offset: '.6' }),
-            //     style({ transformOrigin: 'top left', transform: 'rotate3d(0, 0, 1, 60deg)', animationTimingFunction: 'ease-in-out', opacity: 1, offset: .8, }),
-            //     style({ transform: "translate3d(0, 700px, 0)", opacity: 0, offset: 1 }),
-            //     animate(2000)
-            // ]),
             transition('* => hinge2', animate(2000, keyframes([
                 style({ transformOrigin: 'top left', animationTimingFunction: 'ease-in-out', offset: 0 }),
                 style({ transformOrigin: 'top left', transform: 'rotate3d(0, 0, 1, 80deg)', animationTimingFunction: 'ease-in-out', offset: '0.2' }),
@@ -42,6 +30,14 @@ import { trigger, transition, keyframes, animate, style } from '@angular/animati
                 style({ transformOrigin: 'top left', transform: 'rotate3d(0, 0, 1, 80deg)', animationTimingFunction: 'ease-in-out', offset: '.6' }),
                 style({ transformOrigin: 'top left', transform: 'rotate3d(0, 0, 1, 60deg)', animationTimingFunction: 'ease-in-out', opacity: 1, offset: .8, }),
                 style({ transform: "translate3d(0, 700px, 0)", opacity: 0, offset: 1 }),
+            ]))),
+            transition('* => fadeOut', animate(1000, keyframes([
+                style({ transform: 'translate3d(-100%, 0, 0)', opacity: 0, })
+            ]))),
+            transition('* => zoomOutRight', animate(1000, keyframes([
+                style({ transform: 'scale3d(.475, .475, .475) translate3d(375px, 0, 0) perspective(300px) rotate3d(0, 1, 0, -360deg)', opacity: 0, offset: 0.7 }),
+                style({ transform: 'translate3d(-100%, 0, 0)', opacity: 0, offset: 0.71 }),
+                style({ transform: 'none', opacity: 1, offset: 1 }),
             ]))),
         ])
     ]
